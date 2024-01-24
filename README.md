2161de6bae8806fe45d5788e3fa30ae10aad4ccb
Walmyr <wlsf82@gmail.com>
2024-01-24 22:45:54 +0100

    Create cypress project

---
+++ README.md
@@ -0,0 +1,3 @@
+# dot-then
+
+Sample project to demonstrate Cypress' `.then()` command using the Cypress Playground as the application under test.
---
+++ cypress/e2e/dotThen.cy.js
@@ -0,0 +1,18 @@
+describe('.then()', () => {
+  beforeEach(() => {
+    cy.visit('/index.html')
+  })
+
+  it('copies the code, types it, submits it, then asserts on the susscess message', () => {
+    cy.get('#timestamp')
+      .then($element => {
+        const code = $element[0].innerText
+
+        cy.get('#code').type(code)
+        cy.contains('button', 'Submit').click()
+
+        cy.contains('span', "Congrats! You've entered the correct code.")
+          .should('be.visible')
+      })
+  })
+})
